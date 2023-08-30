cb48ec2b6321bf9e82b96ee483759cad2517fa8a
Ryle Edwards <ryle.edwards@gmail.com>
2023-08-30 11:46:28 -0500

    Add setLength method

--- src/ship.js
+++ src/ship.js
@@ -1,8 +1,15 @@
 class Ship {
   constructor(length, numHits = 0, sunk = false) {
-    this.length = length;
+    this.setLength(length);
     this.numHits = numHits;
     this.sunk = sunk;
+  }
+
+  setLength(newLength) {
+    if (newLength > 0 && newLength < 6) {
+      this.length = newLength;
+    } else
+      throw new Error('Ship length must be greater than 0 and at most 5 units');
   }
 }
 
