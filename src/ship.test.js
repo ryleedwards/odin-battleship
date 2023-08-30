381992206f46438415094814fe817384f0a421e8
Ryle Edwards <ryle.edwards@gmail.com>
2023-08-30 11:47:11 -0500

    Add ship instantiation tests

--- src/ship.test.js
+++ src/ship.test.js
@@ -1,1 +1,29 @@
 const Ship = require('./ship');
+
+describe('Ship instantiation', () => {
+  test('Ship created using only length', () => {
+    const ship = new Ship(3);
+    expect(ship.length).toBe(3);
+    expect(ship.numHits).toBe(0);
+    expect(ship.sunk).toBe(false);
+  });
+
+  test('Ship created with full params given', () => {
+    const ship = new Ship(3, 3, true);
+    expect(ship.length).toBe(3);
+    expect(ship.numHits).toBe(3);
+    expect(ship.sunk).toBe(true);
+  });
+
+  test('Ship too large', () => {
+    expect(() => {
+      const ship = new Ship(6);
+    }).toThrow();
+  });
+
+  test('Ship too small', () => {
+    expect(() => {
+      const ship = new Ship(0);
+    }).toThrow();
+  });
+});
