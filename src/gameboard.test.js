8e38ba5e006c3afc68447317161d341283afb5c4
Ryle Edwards <ryle.edwards@gmail.com>
2023-08-30 15:52:53 -0500

    test row and col in _generateBoard()

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -2,9 +2,21 @@ const Gameboard = require('./gameboard');
 const Ship = require('./ship');
 
 describe('Gameboard creation', () => {
-  const gameboard = new Gameboard();
-  test('gameboard holds 10 rows', () => {
-    expect(gameboard.board.length === 10).toBe(true);
+  const defaultGameboard = new Gameboard();
+  const customGameboard = new Gameboard(8, 9);
+
+  test('gameboard holds correct # of rows', () => {
+    expect(defaultGameboard.board.length).toBe(defaultGameboard.rows);
+    expect(customGameboard.board.length).toBe(customGameboard.rows);
+  });
+
+  test('every gb row holds correct # of cols', () => {
+    defaultGameboard.board.forEach((row) => {
+      expect(row.length).toBe(defaultGameboard.cols);
+    });
+    customGameboard.board.forEach((row) => {
+      expect(row.length).toBe(customGameboard.cols);
+    });
   });
 });
 
