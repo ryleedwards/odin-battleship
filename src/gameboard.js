fa8a7bbad83f6aff941ddbd749fa302dcf331277
Ryle Edwards <ryle.edwards@gmail.com>
2023-08-30 17:06:45 -0500

    Add evaluateCoordinate and placeShip function

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,3 +1,6 @@
+const Ship = require('./ship');
+const tempShip = new Ship(3);
+
 class Gameboard {
   constructor(rows = 10, cols = 10) {
     this.rows = rows;
@@ -15,6 +18,27 @@ class Gameboard {
       rowArr.push(colArr);
     }
     return rowArr;
+  }
+
+  evaluateCoordinate(xCoordinate, yCoordinate) {
+    if (this.board[yCoordinate][xCoordinate] === 'x') return true;
+    else return false;
+  }
+
+  placeShip(xCoordinate, yCoordinate, ship) {
+    // if passed length instead of ship object, it can be assumed
+    // to be a desired length instead
+
+    // perform check on ship arg and if it is not a ship obj, create one
+    if (!(ship instanceof Ship)) {
+      ship = new Ship(ship);
+    }
+
+    for (let i = 0; i < ship.length; i++) {
+      this.board[yCoordinate][xCoordinate + i] = 'x';
+    }
+
+    //TODO - Horizontal vs. Vertical placement
   }
 }
 
