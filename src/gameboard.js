0e17fcf31a853b2af94a064512e1fc60763ee485
Ryle Edwards <ryle.edwards@gmail.com>
2023-08-30 21:59:18 -0500

    Add logic for preventing overlapping ships
    Add occupied array in gameboard constructor
    Modify evaluateCoordinate to refute water rather than look
      for ships
    Add logic for desired placement coordinates
    Add error throw for when desired coordinates are occupied
      by another ship

--- src/gameboard.js
+++ src/gameboard.js
@@ -2,12 +2,13 @@ const Ship = require('./ship');
 const tempShip = new Ship(3);
 
 class Gameboard {
-  constructor(rows = 10, cols = 10, maxShips = 5, ships = []) {
+  constructor(rows = 10, cols = 10, maxShips = 5, ships = [], occupied = []) {
     this.rows = rows;
     this.cols = cols;
     this.board = this._generateBoard(this.rows, this.cols);
     this.maxShips = maxShips;
     this.ships = ships;
+    this.occupied = occupied;
   }
 
   _generateBoard() {
@@ -23,37 +24,54 @@ class Gameboard {
   }
 
   evaluateCoordinate(xCoordinate, yCoordinate) {
-    if (this.board[yCoordinate][xCoordinate] === 'S') return true;
+    if (this.board[yCoordinate][xCoordinate] !== 'W') return true;
     else return false;
   }
 
-  placeShip(xCoordinate, yCoordinate, ship, direction) {
+  placeShip(xCoordinate, yCoordinate, ship) {
     // if passed length instead of ship object, it can be assumed
     // to be a desired length instead
 
     // perform check on ship arg and if it is not a ship obj, create one
     if (!(ship instanceof Ship)) {
-      ship = new Ship(ship);
+      ship = new Ship(ship, this.ships.length);
     }
 
-    // determine horizontal or vertical
+    // what are boat's desired placement coords (ALL SQUARES)
+    const desiredCoords = [];
+    for (let i = 0; i < ship.length; i++) {
+      if (ship.orientation === 'h') {
+        // horizontal
+        desiredCoords.push([xCoordinate, yCoordinate + i]);
+      } else if (ship.orientation === 'v') {
+        // vertical
+        desiredCoords.push([xCoordinate + i, yCoordinate]);
+      }
+    }
 
     // do desired coordinates overlap with other boats?
+    if (this.occupied.length > 0) {
+      desiredCoords.forEach((desiredPair) => {
+        let [xDesired, yDesired] = desiredPair;
+        this.occupied.forEach((occupiedPair) => {
+          let [xOccupied, yOccupied] = occupiedPair;
+          if (xDesired === xOccupied && yDesired === yOccupied)
+            throw new Error(`Desired coordinates at ${set} are occupied`);
+        });
+      });
+    }
 
     // do desired coordinates spill over available board space?
 
     // has maxShips allowed been exceeded?
-    if (this.ships.length < this.maxShips) {
-      // >>NO
-      ship.index = this.ships.length;
-      this.ships.push(ship);
+    if (this.ships.length >= this.maxShips) {
+      throw new Error('Exceeded max number of ships');
     }
-    // >>YES
-    else throw new Error('Exceeded max number of ships');
 
     // All checks pass, proceed with placement
     for (let i = 0; i < ship.length; i++) {
-      this.board[yCoordinate][xCoordinate + i] = 'S';
+      this.board[yCoordinate][xCoordinate + i] = ship.index;
+      this.occupied.push([xCoordinate + i, yCoordinate]);
     }
   }
 
