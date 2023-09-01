82515859bad3ef00670c7e625ba71df765e51398
Ryle Edwards <ryle.edwards@gmail.com>
2023-09-01 10:34:46 -0500

    Test for player attack and hit success

--- src/player.test.js
+++ src/player.test.js
@@ -1,5 +1,6 @@
 const Player = require('./player.js');
 const Gameboard = require('./gameboard.js');
+const Ship = require('./ship.js');
 
 test('player gameboard assignment', () => {
   const player1 = new Player();
@@ -9,10 +10,20 @@ test('player gameboard assignment', () => {
 });
 
 test('Player attack & misses', () => {
-  const player1 = new Player(false);
+  const player1 = new Player();
   player1.assignGameboard(new Gameboard());
-  const player2 = new Player(false);
+  const player2 = new Player();
   player2.assignGameboard(new Gameboard());
 
   expect(player1.attack(player2.gameboard, 5, 5)).toBe(false);
+});
+
+test('Player attacks and hits', () => {
+  const player1 = new Player();
+  player1.assignGameboard(new Gameboard());
+  const player2 = new Player();
+  player2.assignGameboard(new Gameboard());
+  player2.gameboard.placeShip(5, 5, new Ship(3));
+
+  expect(player1.attack(player2.gameboard, 5, 5)).toBe(true);
 });
