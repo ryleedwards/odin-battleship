86241aebdd6b193b266476deca429ed9d37f2635
Ryle Edwards <ryle.edwards@gmail.com>
2023-08-31 21:58:10 -0500

    Add attack function and handle miss

--- src/player.js
+++ src/player.js
@@ -8,6 +8,16 @@ class Player {
   assignGameboard(gameboard) {
     this.gameboard = gameboard;
   }
+
+  attack(targetGameboard, xCoordinate, yCoordinate) {
+    if (targetGameboard.receiveAttack(xCoordinate, yCoordinate)) {
+      // HANDLE HIT
+      return true;
+    } else {
+      // HANDLE MISS
+      return false;
+    }
+  }
 }
 
 module.exports = Player;
