e081e4fb1b63632353bf17b472577705ca0b6068
Ryle Edwards <ryle.edwards@gmail.com>
2023-09-01 11:56:50 -0500

    Add AI subclass with generateAttack method
    AI extends Player class
    generateAttack calculates random x y coords
      and passes those to Player.attack() method

--- src/player.js
+++ src/player.js
@@ -18,4 +18,22 @@ class Player {
   }
 }
 
-module.exports = Player;
+class AI extends Player {
+  constructor(opponentPlayer) {
+    super();
+    this.opponentPlayer = opponentPlayer;
+  }
+
+  generateAttack() {
+    const randomX = Math.floor(
+      Math.random() * this.opponentPlayer.gameboard.cols
+    );
+    const randomY = Math.floor(
+      Math.random() * this.opponentPlayer.gameboard.rows
+    );
+
+    return this.attack(this.opponentPlayer, randomX, randomY);
+  }
+}
+
+module.exports = { Player: Player, AI: AI };
