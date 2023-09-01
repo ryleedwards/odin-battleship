49e3b0e1b19c846c1e5682cd7271fa9ce7a55551
Ryle Edwards <ryle.edwards@gmail.com>
2023-08-31 21:58:22 -0500

    test attack and miss

--- src/player.test.js
+++ src/player.test.js
@@ -7,3 +7,12 @@ test('player gameboard assignment', () => {
   player1.assignGameboard(gameboard1);
   expect(player1.gameboard).toBe(gameboard1);
 });
+
+test('Player attack & misses', () => {
+  const player1 = new Player(false);
+  player1.assignGameboard(new Gameboard());
+  const player2 = new Player(false);
+  player2.assignGameboard(new Gameboard());
+
+  expect(player1.attack(player2.gameboard, 5, 5)).toBe(false);
+});
