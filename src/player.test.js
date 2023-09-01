7d17296a38b1c74159b5b4bd93d5bbc1beb04a6b
Ryle Edwards <ryle.edwards@gmail.com>
2023-08-31 21:48:54 -0500

    Initial commit

---
+++ src/player.test.js
@@ -0,0 +1,20 @@
+const Player = require('./player.js');
+const Gameboard = require('./gameboard.js');
+
+test('player gameboard assignment', () => {
+  const player1 = new Player();
+  const gameboard1 = new Gameboard();
+  player1.assignGameboard(gameboard1);
+  expect(player1.gameboard).toBe(gameboard1);
+});
+
+xtest('Player attack & misses', () => {
+  const player1 = new Player(false);
+  player1.assignGameboard(new Gameboard());
+  const player2 = new Player(false);
+  player2.assignGameboard(new Gameboard());
+
+  player1.attack(player2.gameboard, 5, 5);
+
+  expect();
+});
