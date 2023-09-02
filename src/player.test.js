9f6ae90cedf25a9a3925352ace8387ed75e1e4c0
Ryle Edwards <ryle.edwards@gmail.com>
2023-09-01 19:28:59 -0500

    Add generateAttack > hit test

--- src/player.test.js
+++ src/player.test.js
@@ -59,6 +59,25 @@ describe('AI Behavior', () => {
       expect(player.gameboard.misses.size > 0).toBe(true);
     });
 
-    test('Generates attack >> hit', () => {});
+    test('Generates attack >> hit', () => {
+      const player = new Player();
+      player.assignGameboard(new Gameboard());
+      const ai = new AI(player);
+      ai.assignGameboard(new Gameboard());
+
+      player.gameboard.placeShip(3, 3, 5);
+
+      const totalTiles =
+        ai.opponent.gameboard.rows * ai.opponent.gameboard.cols;
+      let successfulHit = false;
+      for (let i = 0; i < totalTiles; i++) {
+        successfulHit = ai.generateAttack();
+        if (successfulHit === true) {
+          console.log(`Successful AI hit after ${i} attempts`);
+          return;
+        }
+      }
+      expect(successfulHit).toBe(true);
+    });
   });
 });
