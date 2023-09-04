90e32fff3e17582aefae041229312a886834103f
Ryle Edwards <ryle.edwards@gmail.com>
2023-09-04 14:27:27 -0500

    Change renderGameBoard >> renderGameboard

--- src/DOM.js
+++ src/DOM.js
@@ -2,13 +2,13 @@ const dom = (() => {
   const playerBoardDiv = document.querySelector('.board.player');
   const aiBoardDiv = document.querySelector('.board.ai');
 
-  const renderGameBoard = (gbDiv, gb) => {
+  const renderGameboard = (gbDiv, gb) => {
     gb.board.forEach((tile) => {});
   };
 
   const init = (player, ai) => {
-    renderGameBoard(playerBoardDiv, player.gameboard);
-    renderGameBoard(aiBoardDiv, ai.gameboard);
+    renderGameboard(playerBoardDiv, player.gameboard);
+    renderGameboard(aiBoardDiv, ai.gameboard);
   };
   return { init };
 })();
