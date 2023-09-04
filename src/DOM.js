c037260e2c5e94a1d35fd8f25f41f3f9e88fd76d
Ryle Edwards <ryle.edwards@gmail.com>
2023-09-04 17:40:51 -0500

    Add renderGameboard logic for HTML tag creation

--- src/DOM.js
+++ src/DOM.js
@@ -2,13 +2,25 @@ const dom = (() => {
   const playerBoardDiv = document.querySelector('.board.player');
   const aiBoardDiv = document.querySelector('.board.ai');
 
-  const renderGameboard = (gbDiv, gb) => {
-    gb.board.forEach((tile) => {});
+  const renderGameboard = (gbDiv, gb, isOpponent) => {
+    for (let r = 0; r < gb.rows; r++) {
+      const rowDiv = document.createElement('div');
+      rowDiv.classList.add('row');
+      for (let c = 0; c < gb.cols; c++) {
+        const cellDiv = document.createElement('div');
+        cellDiv.classList.add('cell');
+        if (isOpponent) cellDiv.classList.add('opponent');
+        cellDiv.dataset.row = r;
+        cellDiv.dataset.col = c;
+        rowDiv.appendChild(cellDiv);
+      }
+      gbDiv.appendChild(rowDiv);
+    }
   };
 
   const init = (player, ai) => {
-    renderGameboard(playerBoardDiv, player.gameboard);
-    renderGameboard(aiBoardDiv, ai.gameboard);
+    renderGameboard(playerBoardDiv, player.gameboard, false);
+    renderGameboard(aiBoardDiv, ai.gameboard, true);
   };
   return { init };
 })();
