e3749509d8d818e7b895f2e641b66f879131c9a8
Ryle Edwards <ryle.edwards@gmail.com>
2023-09-05 11:38:37 -0500

    Add IIFEs for generating player & ai ships

--- src/game.js
+++ src/game.js
@@ -8,29 +8,33 @@ const dom = require('./DOM');
 const player = new Player();
 const ai = new AI();
 
-// Create and place player ships
-const pShip0 = new Ship(5, 0, 'v');
-player.gameboard.placeShip(2, 2, pShip0);
-const pShip1 = new Ship(4, 0, 'h');
-player.gameboard.placeShip(5, 0, pShip1);
-const pShip2 = new Ship(3, 0, 'h');
-player.gameboard.placeShip(5, 6, pShip2);
-const pShip3 = new Ship(3, 0, 'v');
-player.gameboard.placeShip(1, 0, pShip3);
-const pShip4 = new Ship(2, 0, 'v');
-player.gameboard.placeShip(7, 3, pShip4);
+const generateDefaultPlayerShips = (() => {
+  // Create and place player ships
+  const pShip0 = new Ship(5, 0, 'v');
+  player.gameboard.placeShip(2, 2, pShip0);
+  const pShip1 = new Ship(4, 0, 'h');
+  player.gameboard.placeShip(5, 0, pShip1);
+  const pShip2 = new Ship(3, 0, 'h');
+  player.gameboard.placeShip(5, 6, pShip2);
+  const pShip3 = new Ship(3, 0, 'v');
+  player.gameboard.placeShip(1, 0, pShip3);
+  const pShip4 = new Ship(2, 0, 'v');
+  player.gameboard.placeShip(7, 3, pShip4);
+})();
 
-// Create and place AI ships
-const aiShip0 = new Ship(5, 0, 'v');
-ai.gameboard.placeShip(2, 2, pShip0);
-const aiShip1 = new Ship(4, 0, 'h');
-ai.gameboard.placeShip(5, 0, pShip1);
-const aiShip2 = new Ship(3, 0, 'h');
-ai.gameboard.placeShip(5, 6, pShip2);
-const aiShip3 = new Ship(3, 0, 'v');
-ai.gameboard.placeShip(1, 0, pShip3);
-const aiShip4 = new Ship(2, 0, 'v');
-ai.gameboard.placeShip(7, 3, pShip4);
+const generateDefaultAiShips = (() => {
+  // Create and place AI ships
+  const aiShip0 = new Ship(5, 0, 'v');
+  ai.gameboard.placeShip(2, 2, aiShip0);
+  const aiShip1 = new Ship(4, 0, 'h');
+  ai.gameboard.placeShip(5, 0, aiShip1);
+  const aiShip2 = new Ship(3, 0, 'h');
+  ai.gameboard.placeShip(5, 6, aiShip2);
+  const aiShip3 = new Ship(3, 0, 'v');
+  ai.gameboard.placeShip(1, 0, aiShip3);
+  const aiShip4 = new Ship(2, 0, 'v');
+  ai.gameboard.placeShip(7, 3, aiShip4);
+})();
 
 // Initialize DOM
 dom.init(player, ai);
